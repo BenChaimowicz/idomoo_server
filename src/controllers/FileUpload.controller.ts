2b9491fcad06daafb6959f8a240015cad8fa3c97
Ben Chaimowicz <doroza@gmail.com>
2023-09-20 23:32:17 +0300

    changed image upload to cloudinary

--- src/controllers/FileUpload.controller.ts
+++ src/controllers/FileUpload.controller.ts
@@ -1,7 +1,8 @@
 import { Service } from "typedi";
 import { NextFunction, Request, Response } from "express";
 import { UploadedFile } from "express-fileupload";
-import path from 'path';
+import { v2 as cloudinary } from 'cloudinary';
+import { createReadStream } from 'streamifier';
 
 
 @Service()
@@ -11,12 +12,22 @@ export class FileUploadController {
     public async uploadFile(req: Request, res: Response, next: NextFunction): Promise<void> {
         try {
             const file = req.files!.image as UploadedFile;
-            const savePath = `${path.resolve(__dirname, '../')}/uploads/1.jpg`;
-            file.mv(savePath, (err) => {
-                if (err) return res.status(500).send(err);
-                const imageURL = '/uploads/1.jpg';
-                res.send(imageURL);
+            if (!file) throw new Error('No file');
+            const { CLOUD_NAME, API_KEY, API_SECRET } = process.env;
+
+            cloudinary.config({
+                cloud_name: CLOUD_NAME,
+                api_key: API_KEY,
+                api_secret: API_SECRET
             });
+            const uploadStream = cloudinary.uploader.upload_stream({ folder: 'idomoo', overwrite: true }, function (e, r) {
+                if (e) return res.status(500).send({ msg: e.message, code: e.http_code, name: e.name });
+                console.log(r);
+                res.send(r?.secure_url);
+            });
+            createReadStream(file.data).pipe(uploadStream)
+
+            uploadStream.addListener('close', () => console.log('upload finished'));
         } catch (error) {
             res.send(error);
         }
